36780812ca84b899417ac7ef8a23b83ec632cd84
Jasper Cacbay <jasper.cacbay@gmail.com>
2013-09-22 21:32:40 +0800

    ui views 1 and 2 (w/ activity transition)

---
+++ Test/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void viewNextDay(View view) {
+        Intent intent = new Intent(this, WeekViewActivity.class);
+        startActivity(intent);
+    }
+
+    public void searchCity(View view) {
+        Intent intent = new Intent(this, SearchViewActivity.class);
+        startActivity(intent);
+    }
+}
